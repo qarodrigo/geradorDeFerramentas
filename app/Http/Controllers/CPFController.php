8118e612a7d658c811948018d61050ff6c9d2fab
Rodrigo Santos <rodrigo.contato1@outlook.com>
2021-05-19 15:14:25 -0300

    cpf controller

---
+++ app/Http/Controllers/CPFController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class CPFController extends Controller
+{
+    public function primeirosDigitos()
+    {
+        $primeirosDigitos = [];
+
+        for ($i = 0; $i < 9; $i++) {
+            $primeirosDigitos[$i] = rand(0, 9);
+        }
+        return $primeirosDigitos;
+    }
+
+    public function gerarNumerosDeValidacao($primeirosDigitos)
+    {
+
+        $total = 0;
+        $base = sizeof($primeirosDigitos) == 10 ? 11 : 10;
+
+        if (sizeof($primeirosDigitos) == 11) {
+            return $primeirosDigitos;
+        }
+
+        foreach ($primeirosDigitos as $item) {
+            $total += $item * $base;
+            $base--;
+        }
+
+        $resto = $total % 11;
+        $resto < 2 ?  $primeirosDigitos[] = 0 : $primeirosDigitos[] = 11 - $resto;
+
+        return $this->gerarNumerosDeValidacao($primeirosDigitos);
+    }
+
+    public function geraCPF()
+    {
+
+        $primeirosDigitos = $this->primeirosDigitos();
+        $cpf = $this->gerarNumerosDeValidacao($primeirosDigitos);
+        return response()->json(implode($cpf));
+    }
+
+
+    public function validaCPF($cpf)
+    {
+        $primeirosDigitos = str_split($cpf);
+        $primeirosDigitos = array_slice($primeirosDigitos, 0, -2);
+        $validado = $this->gerarNumerosDeValidacao($primeirosDigitos);
+
+        return response()->json(implode($validado) == $cpf ? true : false);
+    }
+}
